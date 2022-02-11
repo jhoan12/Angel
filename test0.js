bc35ac65a2801b8c2dd309eaed35c6a2ae9677f6
alexis129 <jhoan.alvarez@models1a.com>
2022-02-10 19:54:01 -0500

    10/02/2022

---
+++ test0.js
@@ -0,0 +1,29 @@
+// A continuación se muestra un arreglo alumnos con datos básicos de cada alumno, y en el arreglo emails se muestra los emails de cada alumno.
+
+// Deberá crear una función donde se devuelva un nuevo arreglo con los datos básicos de cada alumno con el email agregado.
+
+const alumnos = [
+  {
+    id: 13,
+    firstname: "MARIA CATALINA",
+    lastname: "ALVAREZ HENAO",
+    idnumber: 1152683727,
+  },
+  {
+    id: 26,
+    firstname: "KAREN LIVETH",
+    lastname: "ALVAREZ TOBÓN",
+    idnumber: 1038125952,
+  },
+];
+
+const emails = [
+  {
+    id: 26,
+    email: "qarentobon@gmail.com",
+  },
+  {
+    id: 13,
+    email: "caticas99@gmail.com",
+  },
+];
---
+++ test1.js
@@ -0,0 +1,17 @@
+// concatenar el array names con el array lastNames
+// Considera una lista/array de ovejas. Cada oveja tiene un nombre y un color. Haz una función que devuelva una lista con todas las ovejas que sean de color rojo y que además su nombre contenga tanto las letras n Y a, sin importar el orden, las mayúsculas o espacios.
+
+// Por ejemplo, si tenemos las ovejas:
+
+// const ovejas = [
+//   { name: "Noa", color: "azul" },
+//   { name: "Euge", color: "rojo" },
+//   { name: "Navidad", color: "rojo" },
+//   { name: "Ki Na Ma", color: "rojo" },
+//   { name: "AAAAAaaaaa", color: "rojo" },
+//   { name: "Nnnnnnnn", color: "rojo" },
+// ];
+
+// devería devolver:
+//[{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
